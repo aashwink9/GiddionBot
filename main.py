e1b80f18b114efd1f20195688f1725d22645ee5b
Aashwin Katiyar <akflash9@gmail.com>
2021-11-26 21:49:08 -0500

    Created configurations for heroku - MAIN BRANCH

--- main.py
+++ main.py
@@ -1,36 +1,48 @@
+import json
+import os
 import discord
 from discord.ext import commands
 import random
 import music
 
+# --------------FOR LOCAL RUN--------------
+# credsf = open("creds.json")
+# creds = json.load(credsf)
+# TOKEN = creds["token"]
+# -----------------------------------------
+
+# -------------FOR HEROKU------------------
+TOKEN = os.getenv("DISCORD_TOKEN")
+# -----------------------------------------
 
 cogs = [music]
 
-client = commands.Bot(command_prefix='-', intents = discord.Intents.all())
+client = commands.Bot(command_prefix='-', intents=discord.Intents.all())
 
 for i in range(len(cogs)):
     cogs[i].setup(client)
 
-def jokes():
-    jokes = ["I ate a clock yesterday, it was very time-consuming.",
-             "Have you played the updated kids' game? I Spy With My Little Eye . . . Phone.",
-             "A perfectionist walked into a bar...apparently, the bar wasn’t set high enough.",
-             ]
 
-    return random.choice(jokes)
+def jokes():
+    joke = ["I ate a clock yesterday, it was very time-consuming.",
+            "Have you played the updated kids' game? I Spy With My Little Eye . . . Phone.",
+            "A perfectionist walked into a bar...apparently, the bar wasn’t set high enough.",
+            ]
+
+    return random.choice(joke)
 
 
 def allcommands(com):
     if com == "greeting":
-        intro_mssg = "Hi I'm Giddion, Aashwin's proud creation. Nice to meet you! I can crack jokes, or generate a random " \
-                     "number for you. Simply type 'crack jokes' if you want me to crack jokes, or 'generate num' if you want " \
-                     "me to generate a random number between 1 and 100. "
+        intro_mssg = "Hi I'm Giddion, Aashwin's proud creation. Nice to meet you! I can crack jokes, or generate a " \
+                     "random number for you. Simply type 'crack jokes' if you want me to crack jokes, or " \
+                     "'generate num' if you want me to generate a random number between 1 and 100. "
         return intro_mssg
 
-        
+
 @client.event
 async def on_ready():
     print('logged in! as {0.user}'.format(client))
 
 
-client.run('ODQ0NDYwMzAyODIxMjI4NTc1.YKSu8Q.ZvhDRb51nUFi2CFJCPEnqq_-KKw')
+client.run(TOKEN)
--- music.py
+++ music.py
@@ -1,78 +1,224 @@
 import discord
 from discord.ext import commands
 import youtube_dl
-import random
+import asyncio
+
+FFMPEG_OPTIONS = {
+    "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",
+    "options": "-vn",
+}
+
+YTDL_FORMAT_OPTIONS = {
+    "format": "bestaudio",
+    "outtmpl": "%(extractor)s-%(id)s-%(title)s.%(ext)s",
+    "restrictfilenames": True,
+    "noplaylist": True,
+    "nocheckcertificate": True,
+    "ignoreerrors": True,
+    "logtostderr": False,
+    "quiet": True,
+    "no_warnings": True,
+    "default_search": "auto",
+    "source_address": "0.0.0.0",
+}
+
+ytdl = youtube_dl.YoutubeDL(YTDL_FORMAT_OPTIONS)
+
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+
+        self.title = data.get("title")
+        self.url = data.get("url")
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False, play=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(
+            None, lambda: ytdl.extract_info(url, download=False)
+        )
+
+        if "entries" in data:
+            data = data["entries"][0]
+
+        filename = data["url"] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **FFMPEG_OPTIONS), data=data)
 
 
 class music(commands.Cog):
     def __init__(self, client):
         self.client = client
-
-    @commands.command()
-    async def join(self, ctx):
-        if ctx.author.voice is None:
-            await ctx.send("You're not in a voice channel!")
-        voice_channel = ctx.author.voice.channel
-        if ctx.voice_client is None:
-            await voice_channel.connect()
-        else:
-            await ctx.voice_client.move_to(voice_channel)
-
-
-    @commands.command()
-    async def dc(self, ctx):
-        await ctx.voice_client.disconnect()
-
-    @commands.command()
-    async def disconnect(self, ctx):
-        await ctx.voice_client.disconnect()
+        self.queue = []
 
     @commands.command()
     async def play(self, ctx, url):
-      voice_channel = ctx.author.voice.channel
-      await voice_channel.connect()
-      FFMPEG_OPTIONS = {
-        'before_options':
-        '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
-        'options': '-vn'
-        }
-      YDL_OPTIONS = {'format': "bestaudio"}
-      vc = ctx.voice_client
-      with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
-        info = ydl.extract_info(url, download=False)
-        url2 = info['formats'][0]['url']
-        source = await discord.FFmpegOpusAudio.from_probe(
-            url2, **FFMPEG_OPTIONS)
-        vc.play(source)
+        channel = ctx.message.author.voice.channel
+        voice = discord.utils.get(self.client.voice_clients, guild=ctx.guild)
+        song = await YTDLSource.from_url(url, loop=self.client.loop, stream=True)
+        position = len(self.queue) + 1
 
-    
-    @commands.command()
-    async def p(self, ctx, url):
-      voice_channel = ctx.author.voice.channel
-      await voice_channel.connect()
-      FFMPEG_OPTIONS = {
-        'before_options':
-        '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
-        'options': '-vn'
-        }
-      YDL_OPTIONS = {'format': "bestaudio"}
-      vc = ctx.voice_client
-      with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
-        info = ydl.extract_info(url, download=False)
-        url2 = info['formats'][0]['url']
-        source = await discord.FFmpegOpusAudio.from_probe(
-            url2, **FFMPEG_OPTIONS)
-        vc.play(source)
+        if not ctx.message.author.voice:
+            await ctx.send("You are not connected to a voice channel!")
+            return
+
+        elif voice is not None:
+            if not voice.is_playing():
+                try:
+                    self.queue.append(song)
+
+                    while len(self.queue) > 0:
+                        ctx.voice_client.play(self.queue.pop(0),
+                                              after=lambda e: print("Player error:   %s" % e) if e else None, )
+
+                    await ctx.send(
+                        f":mag_right: **Searching for** "
+                        "*" + url + "*"
+                        + "\n<:arrow_forward:763374159567781890> **Now Playing: ** ``{}".format(
+                            song.title
+                        )
+                        + "``"
+                    )
+
+                except:
+                    await ctx.send("Something went wrong - please try again later!")
+
+            else:
+                try:
+                    self.queue.append(song)
+                    await ctx.send(
+                        f":mag_right: **Searching for** "
+                        + "*" + url + "*"
+                        + "\n<:play_pause:763374159567781890> **Queued Song: ** ``{}".format(
+                            song.title
+                        )
+                        + "``"
+                        + " **At Position ("
+                        + str(position)
+                        + ")**"
+                    )
+
+                except:
+                    await ctx.send("Something went wrong - please try again later!")
+
+        else:
+            await channel.connect()
+            try:
+                self.queue.append(song)
+                ctx.voice_client.play(self.queue.pop(0),
+                                      after=lambda e: print("Player error:   %s" % e) if e else None, )
+
+                await ctx.send(
+                    f":mag_right: **Searching for** "
+                    "*" + url + "*"
+                    + "\n<:arrow_forward:763374159567781890> **Now Playing: ** ``{}".format(
+                        song.title
+                    )
+                    + "``"
+                )
+
+            except:
+                await ctx.send("Something went wrong - please try again later!")
+
+    # def play_next(self, ctx, source):
+    #   if len(self.queue) >= 1:
+
+    #     voice = discord.utils.get(self.client.voice_clients, guild=ctx.guild)
+    #     voice.play(discord.FFmpegPCMAudio(source=source, after=lambda e: play_next(self, ctx, source)))
 
     @commands.command()
     async def pause(self, ctx):
         await ctx.voice_client.pause()
-        await ctx.send("Paused!")
+        await ctx.send(":pause_button: Paused!")
 
     @commands.command()
     async def resume(self, ctx):
         await ctx.voice_client.resume()
-        await ctx.send("resumed!")
+        await ctx.send(":arrow_forward: resumed!")
+
+    @commands.command()
+    async def leave(self, ctx):
+        voice_client = ctx.message.guild.voice_client
+        user = ctx.message.author.mention
+        await voice_client.disconnect()
+        await ctx.send(f"Disconnected by {user}")
+
+    @commands.command()
+    async def dc(self, ctx):
+        voice_client = ctx.message.guild.voice_client
+        user = ctx.message.author.mention
+        await voice_client.disconnect()
+        await ctx.send(f"Disconnected by {user}")
+
+    @commands.command()
+    async def disconnect(self, ctx):
+        voice_client = ctx.message.guild.voice_client
+        user = ctx.message.author.mention
+        await voice_client.disconnect()
+        await ctx.send(f"Disconnected by {user}")
+
+    @commands.command()
+    async def reset(self, ctx):
+        voice_client = ctx.message.guild.voice_client
+        self.queue.clear()
+        await ctx.send(":gun: The queue has been reset! Exiting...")
+        await voice_client.disconnect()
+
+    @commands.command()
+    async def clearq(self, ctx):
+        self.queue.clear()
+        await ctx.send(":gun: The queue has been cleared!")
+
+    @commands.command()
+    async def skip(self, ctx):
+        voice_client = ctx.message.guild.voice_client
+        if len(self.queue) == 0:
+            await ctx.send(":stop_sign: *Stopping...\nLooks like you've at the end of the queue!*")
+            voice_client.stop()
+            return
+        else:
+            voice_client.stop()
+            nextsong = self.queue.pop(0)
+            await ctx.send(":fast_forward: *Skipping!*\n**Playing:** " + "``" + str(nextsong.title) + "``")
+
+            ctx.voice_client.play(nextsong)
+            return
+
+    @commands.command()
+    async def next(self, ctx):
+        voice_client = ctx.message.guild.voice_client
+        if len(self.queue) == 0:
+            await ctx.send(":stop_sign: *Stopping...\nLooks like you've at the end of the queue!*")
+            voice_client.stop()
+            return
+        else:
+            voice_client.stop()
+            nextsong = self.queue.pop(0)
+            await ctx.send(":fast_forward: *Skipping!*\n**Playing:** " + "``" + str(nextsong.title) + "``")
+
+            ctx.voice_client.play(nextsong)
+            return
+
+    @commands.command()
+    async def stop(self, ctx):
+        voice_client = ctx.message.guild.voice_client
+        voice_client.stop()
+        await ctx.send(":stop_sign: *Stopped!*")
+
+    @commands.command()
+    async def show(self, ctx):
+
+        if len(self.queue) == 0:
+            await ctx.send("The queue is empty!")
+            return
+
+        qstr = ""
+        for q in self.queue:
+            qstr += "```" + str(self.queue.index(q) + 1) + ". " + str(q.title) + "```"
+
+        await ctx.send("**Songs in queue:**\n" + qstr)
 
 
 def setup(client):
