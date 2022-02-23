fa33ddf2c779d7760fd3526a863cf0ca8c977d13
aashwink9 <46697015+aashwink9@users.noreply.github.com>
2022-02-22 19:42:59 -0500

    Changed the prefix to '='

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 
 cogs = [music]
 
-client = commands.Bot(command_prefix='-', intents=discord.Intents.all())
+client = commands.Bot(command_prefix='=', intents=discord.Intents.all())
 
 for i in range(len(cogs)):
     cogs[i].setup(client)
